cd87851ea433c7fe0d90b1c641d3b11dc18af87d
danpatoneil <danpatoneil@gmail.com>
2024-06-19 16:08:39 -0400

    added PWA functionality and DB routes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,37 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) =>{
+    console.log('putDB');
+    //open DB connection
+    const jateDB = await openDB('jate', 1);
+    //create a new transaction
+    const tx = jateDB.transaction('jate', 'readwrite');
+    //open desired object store
+    const store = tx.objectStore('jate');
+    //add content to database
+    const request = store.add(content);
+    //get confirmation
+    const result = await request;
+    console.log('content saved to database', result)
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+    console.log('getDB');
+    //open connection
+    const jateDB = await openDB('jate', 1);
+    //create a new transaction
+    const tx = jateDB.transaction('jate', 'readwrite');
+    //open desired object store
+    const store = tx.objectStore('jate');
+    //get all content from store
+    const request = store.getAll();
+    //get confirmation
+    const result = await request;
+    console.log('result: ', result);
+    //return it
+    return result;
+};
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,50 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: "It's kinda in the title bud",
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+            {
+                src: path.resolve('src/images/logo.png'),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join('assets', 'icons'),
+            }
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+              },
+            },
+          },
       ],
     },
   };
